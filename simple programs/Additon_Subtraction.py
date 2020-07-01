9b14dc91cf1fa198cbec5bde1ada9824304ed2c2
rahul-singh-au9 <rahulsg1508@gmail.com>
2020-07-01 23:16:10 +0530

    simple programs

---
+++ simple programs/Additon_Subtraction.py
@@ -0,0 +1,8 @@
+n1=int(input("please enter first number"))
+n2=int(input("please enter second number"))
+print("sum of both number=" , n1+n2)
+    
+print("differnce of both number=", n1-n2)
+print("product of both number=", n1*n2)
+
+
---
+++ simple programs/Division.py
@@ -0,0 +1,10 @@
+number1=int(input("please enter first number"))
+number2=int(input("please enter second number"))
+division= number1//number2
+
+print("interger division =", division)
+
+
+division= number1/number2
+
+print("float division=", division)
