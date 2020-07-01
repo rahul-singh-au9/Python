b8f4810cb365279ef87281152ede63fc16693b64
rahul-singh-au9 <rahulsg1508@gmail.com>
2020-07-01 23:13:47 +0530

    simple loops

---
+++ loops/For_loop.py
@@ -0,0 +1,7 @@
+n=int(input())
+if(0<n<20):
+    for i in range(0,n):
+        print(i*i)
+    else:
+        print("exit")
+        
---
+++ loops/While_loop.py
@@ -0,0 +1,8 @@
+number=1
+while number<=150:
+
+    print(number, end="")
+
+    number+=1
+    
+
